2348ffaf63664c533192a6c690ab293fe512dd72
Ryusaikou <Ryusaikou2049@gmail.com>
2019-05-13 21:35:17 -0700

    Initial Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './core/authentication/authentication.component';
+import { GuardsComponent } from './core/guards/guards.component';
+import { HttpComponent } from './core/http/http.component';
+import { InterceptorsComponent } from './core/interceptors/interceptors.component';
+import { ServicesComponent } from './core/services/services.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    GuardsComponent,
+    HttpComponent,
+    InterceptorsComponent,
+    ServicesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
